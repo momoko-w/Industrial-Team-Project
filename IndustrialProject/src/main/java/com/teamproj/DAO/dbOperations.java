cfaed3772683fb342f005a98831b0b5693669b94
abbas67 <androidabbas@googlemail.com>
2019-09-16 22:24:11 +0100

    created basic project template

---
+++ IndustrialProject/src/main/java/com/teamproj/DAO/dbOperations.java
@@ -0,0 +1,13 @@
+package com.teamproj.DAO;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class dbOperations {
+
+
+
+
+
+}
---
+++ IndustrialProject/src/main/java/com/teamproj/Main.java
@@ -0,0 +1,4 @@
+package com.teamproj;
+
+public class Main {
+}
